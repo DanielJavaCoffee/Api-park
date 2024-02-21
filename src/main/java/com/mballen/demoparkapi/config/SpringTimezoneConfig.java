42df9d9b85cbe8cc82809bdc157e083eed924ae1
daniel <danielsilvacr123@gmail.com>
2024-02-21 01:21:55 -0300

    chore: Implementação da conexão com o banco de dados

--- src/main/java/com/mballen/demoparkapi/config/SpringTimezoneConfig.java
+++ src/main/java/com/mballen/demoparkapi/config/SpringTimezoneConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import java.util.TimeZone;
 
 @Configuration
-public class SpringTimeZoneConfig {
+public class SpringTimezoneConfig {
 
     @PostConstruct
-    public void timeZoneConfig(){
+    public void timezoneConfig(){
         TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
     }
 }
